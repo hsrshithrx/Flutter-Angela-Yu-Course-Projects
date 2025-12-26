90765e5958960c7267982400c6a7f11c55dd741b
harshit Rawat <harshit.rawatgt@gmail.com>
2025-12-26 23:59:14 +0530

    Add firebase.json to gitignore and remove from tracking

--- todoey_flutter/lib/widgets/task_tile.dart
+++ todoey_flutter/lib/widgets/task_tile.dart
@@ -1,37 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+
 
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: TaskCheckbox(),
+      title: Text('This is a task',
+        style: TextStyle(decoration: isChecked! ? TextDecoration.lineThrough : null),),
+      trailing: TaskCheckbox(checkboxState: isChecked,
+          toggleCheckboxState: (bool? checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
+class TaskCheckbox extends StatelessWidget {
 
-  @override
-  _TaskCheckboxState createState() => _TaskCheckboxState();
-}
+  final bool? checkboxState;
+  final ValueChanged<bool?>  toggleCheckboxState;
 
-class _TaskCheckboxState extends State<TaskCheckbox>{
-
-  bool? isChecked = false;
+  TaskCheckbox({this.checkboxState, required this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
         activeColor: Colors.lightBlueAccent,
-        value: isChecked ,
-        onChanged: (bool? newValue){
-          setState(() {
-            isChecked = newValue;
-          });
-        }
+        value: checkboxState ,
+        onChanged: toggleCheckboxState,
     );
   }
 }
